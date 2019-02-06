35a08b07f4b081aebd5e4d81d13f2942d2731c63
Lars Holmberg <lars.holmberg@svenskaspel.se>
2019-02-06 15:08:19 +0100

    Added some influxdb and websocket craziness.

--- build.sbt
+++ build.sbt
@@ -1,10 +1,11 @@
 enablePlugins(GatlingPlugin)
 
-scalaVersion := "2.12.8"
+scalaVersion := "2.12.7"
 
 scalacOptions := Seq(
   "-encoding", "UTF-8", "-target:jvm-1.8", "-deprecation",
   "-feature", "-unchecked", "-language:implicitConversions", "-language:postfixOps")
 
 libraryDependencies += "io.gatling.highcharts" % "gatling-charts-highcharts" % "3.0.3" % "test,it"
-libraryDependencies += "io.gatling"            % "gatling-test-framework"    % "3.0.3" % "test,it"
+libraryDependencies += "io.gatling"            % "gatling-test-framework"    % "3.0.4-SNAPSHOT" % "test,it"
+libraryDependencies += "com.paulgoldbaum" %% "scala-influxdb-client" % "0.6.1"
--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -2,51 +2,65 @@ package computerdatabase
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import scala.concurrent.duration._
+
+import io.gatling.core.session.Session
+import com.paulgoldbaum.influxdbclient._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+// object Predef {
+//  def justDoIt(param: String): Session => Validation[Session] = s => s.set("some", param)
+// }
+
 
 class BasicSimulation extends Simulation {
 
-  val httpProtocol = http
-    .baseUrl("http://computer-database.gatling.io") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+  val influxdb = InfluxDB.connect("influx_hostname", 8086)
+  val database = influxdb.selectDatabase("gatling_raw_requests")
+  database.create()
+  
+  val scenarioName = "Mix"
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("request_1")
-      .get("/"))
-    .pause(7) // Note that Gatling has recorder real time pauses
-    .exec(http("request_2")
-      .get("/computers?f=macbook"))
-    .pause(2)
-    .exec(http("request_3")
-      .get("/computers/6"))
-    .pause(3)
-    .exec(http("request_4")
-      .get("/"))
-    .pause(2)
-    .exec(http("request_5")
-      .get("/computers?p=1"))
-    .pause(670 milliseconds)
-    .exec(http("request_6")
-      .get("/computers?p=2"))
-    .pause(629 milliseconds)
-    .exec(http("request_7")
-      .get("/computers?p=3"))
-    .pause(734 milliseconds)
-    .exec(http("request_8")
-      .get("/computers?p=4"))
-    .pause(5)
-    .exec(http("request_9")
-      .get("/computers/new"))
-    .pause(1)
-    .exec(http("request_10") // Here's an example of a POST request
-      .post("/computers")
-      .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
-      .formParam("""introduced""", """2012-05-30""")
-      .formParam("""discontinued""", """""")
-      .formParam("""company""", """37"""))
+  val scn = scenario(scenarioName)
+    .exec(ws("connect ws").connect("/socket.io/?EIO=3&transport=websocket")
+      .await(10)(ws.checkTextMessage("sid_received").check(regex(""""sid":"\w*"""").saveAs("name"))))
+    .exec { session =>
+      println(session("name").as[String])
+      session
+    } 
+    .exec(ws("message2").sendText("""42["subscribe",{"url":"/sport/matches/1353783/odds","sendInitialUpdate":true}]""")
+      .await(20)(ws.checkTextMessage("initial_odds_received").check(regex("""42\["update",\{"path":"/sport/matches/1353783/odds".*""").saveAs("initial_odds"))))
+    .exec { session =>
+      println(session("initial_odds").as[String])
+      session
+    }
+    .exec(ws("Close WS").close)
+
+  val httpProtocol = http
+    .wsBaseUrl("wss://hostname")
+    
+    
+  // val scn = scenario(scenarioName)
+  //   // .exec(http("request_get")
+  //   //   .get("/").check( responseTimeInMillis.saveAs("responseTime") )
+  //   // )
+  //   .exec(ws("connect ws").connect("/socket.io/?EIO=3&transport=websocket&sid=BpvegaGfaVW88hbVAVHm"))
+
+  // val httpProtocol = http
+  //   .baseUrl("https://hostname")
+  //   .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+  //   .acceptEncodingHeader("gzip, deflate")
+  //   .acceptLanguageHeader("en-US,en;q=0.5")
+  //   .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+  //   .check(responseTimeInMillis.transform(responseTime => {
+  //     database.write(new Point("gatling_requests")
+  //         .addTag("scenarioName", scenarioName)
+  //         //.addTag("name", )
+  //         .addField("responseTime", responseTime))
+  //         responseTime
+  //     }
+  //   ).saveAs("nevermind"))
+  //   .wsBaseUrl("wss://hostname")
+    //.header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits")
 
   setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
 }
